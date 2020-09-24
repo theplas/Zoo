7e62219b132d125211a0aec97fb40dde9d2abedd
theplas <kanicepakulate@gmail.com>
2020-09-24 08:59:53 +0700

    first commit

---
+++ Animal.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZOO_MANAGEMENT_SYSTEM
+{
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Description { get; set; }
+        public virtual void ViewInfo()
+        {
+            Console.WriteLine("Ten dong vat : {0} , Tuoi : {1} , Mo ta : {2}", Name, Age, Description);
+        }
+        public abstract void Speak();
+
+        public Animal(string name, int age, string description)
+        {
+            Name = name;
+            Age = age;
+            Description = description;
+        }
+
+        public Animal()
+        {
+        }
+    }
+}
---
+++ Dog.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZOO_MANAGEMENT_SYSTEM
+{
+    public class Dog : Animal
+    {
+
+        public override void Speak()
+        {
+            Console.WriteLine("Gau Gau");
+        }
+        public Dog() : base()
+        {
+
+        }
+        public override void ViewInfo()
+        {
+            base.ViewInfo();
+        }
+        public Dog(string Name, int Age, string Description) : base(Name, Age, Description)
+        {
+        }
+    }
+}
